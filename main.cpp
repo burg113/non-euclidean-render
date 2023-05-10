ba9a173cf1f5cb3fbe089e61dfd26aa25a76939c
Burg <burg113@gmx.net>
2023-05-10 18:17:51 +0200

    some work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include "lib/toml.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+toml::table readToml(std::string path) {
+    toml::table tbl;
+    try
+    {
+        tbl = toml::parse_file(path);
+        std::cout << tbl << "\n";
+    }
+    catch (const toml::parse_error& err)
+    {
+        std::cerr << "Parsing failed:\n" << err << "\n";
+    }
+    return tbl;
+}
+
+
+
+int main(int argc, char** argv) {
+    string fileDir;
+    if (argc == 0) {
+        cout << "please input the path to the scene file: \n" << endl;
+        cin >> fileDir;
+    }else
+        fileDir = argv[1];
+
+
+    toml::table t = readToml(fileDir);
+
+
+
+
     return 0;
 }
