5016b0b331d0174b9816cd720cf2bb87066959a7
Burg <burg113@gmx.net>
2023-05-20 11:54:24 +0200

    move to correct location

--- lib/stbi_impl.cpp
+++ lib/stbi_impl.cpp
@@ -1,4 +1,4 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "lib/stb_image.h"
-#include "lib/stb_image_write.h"
+#include "stb_image.h"
+#include "stb_image_write.h"
